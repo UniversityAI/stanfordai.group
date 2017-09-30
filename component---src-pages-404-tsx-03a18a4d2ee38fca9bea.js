6381e7b4bb4190bf19851642d5bc31f3d2704baa
Andrew Lee <andrewsoohwanlee@gmail.com>
2017-09-30 12:21:03 -0700

    Updates

---
+++ component---src-pages-404-tsx-03a18a4d2ee38fca9bea.js
@@ -0,0 +1,24 @@
+webpackJsonp([178469467687848],{
+
+/***/ "./src/pages/404.scss":
+/***/ (function(module, exports) {
+
+	// empty (null-loader)
+
+/***/ }),
+
+/***/ "./node_modules/babel-loader/lib/index.js?{\"plugins\":[\"/Users/asl/Desktop/saig/node_modules/gatsby/dist/utils/babel-plugin-extract-graphql.js\"]}!./node_modules/ts-loader/index.js?{\"compilerOptions\":{\"target\":\"esnext\",\"experimentalDecorators\":true,\"jsx\":\"react\",\"module\":\"commonjs\"},\"transpileOnly\":true}!./src/pages/404.tsx":
+/***/ (function(module, exports, __webpack_require__) {
+
+	"use strict";
+	
+	Object.defineProperty(exports, "__esModule", { value: true });
+	const React = __webpack_require__("./node_modules/gatsby-plugin-react-next/node_modules/react/index.js");
+	__webpack_require__("./src/pages/404.scss");
+	const NotFoundPage = () => React.createElement("div", { className: "pageNotFound__container" }, React.createElement("h2", { className: "pageNotFound__subtitle" }, "404"), React.createElement("h2", { className: "pageNotFound__title" }, "Page Not Found"), React.createElement("p", null, "You just hit a route that doesn't exist."), React.createElement("p", null, "\uD83D\uDEA7"));
+	exports.default = NotFoundPage;
+
+/***/ })
+
+});
+//# sourceMappingURL=component---src-pages-404-tsx-03a18a4d2ee38fca9bea.js.map
